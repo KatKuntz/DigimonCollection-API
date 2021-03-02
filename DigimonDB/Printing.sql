d85606ae3da3c0b48dab34ff7680ebd51158fc88
Kat Kuntz <kat.kuntz.dev@gmail.com>
2021-03-02 10:06:47 -0500

    Finish card printing DB

---
+++ DigimonDB/Printing.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Printing]
+(
+	[Id] INT NOT NULL  IDENTITY,
+    [AlternateArt] BIT NOT NULL DEFAULT 0,
+    CONSTRAINT [PK_Printing] PRIMARY KEY ([Id])
+)
--- DigimonDB/Set.sql
+++ DigimonDB/Set.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Set]
 (
-	[Id] NCHAR(10) NOT NULL , 
-    [Name] NVARCHAR(50) NOT NULL, 
-    [ReleaseDate] DATE NOT NULL, 
+	[Id] NCHAR(10) NOT NULL ,
+    [Name] NVARCHAR(50) NOT NULL,
+    [ReleaseDate] DATE NOT NULL,
     CONSTRAINT [PK_Set] PRIMARY KEY ([Id])
 )
