5a22f7eb99344d1867b588f0b5ba7e442cd43459
Kat Kuntz <kat.kuntz.dev@gmail.com>
2021-03-03 00:03:25 -0500

    Modify string columns in db

--- DigimonDB/Card.sql
+++ DigimonDB/Card.sql
@@ -1,11 +1,11 @@
 ﻿CREATE TABLE [dbo].[Card]
 (
 	[Id] INT NOT NULL,
-    [SetId] NCHAR(10) NOT NULL,
+    [SetId] NVARCHAR(10) NOT NULL,
     [Number] INT NOT NULL,
     [Name] NVARCHAR(50) NOT NULL,
     [Color] VARCHAR(16) NOT NULL,
-    [Type] CHAR(16) NOT NULL,
+    [Type] VARCHAR(16) NOT NULL,
     CONSTRAINT [FK_Card_ToSet] FOREIGN KEY ([SetId]) REFERENCES [Set]([Id]),
     CONSTRAINT [CK_Card_Color] CHECK (Color IN ('Red','Yellow','Blue','Green','Purple','Black','White')),
     CONSTRAINT [CK_Card_Type] CHECK (Type IN ('Option','Tamer','Digimon','Digi-Egg')),
--- DigimonDB/Set.sql
+++ DigimonDB/Set.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Set]
 (
-	[Id] NCHAR(10) NOT NULL ,
+	[Id] NVARCHAR(10) NOT NULL ,
     [Name] NVARCHAR(50) NOT NULL,
     [ReleaseDate] DATE NOT NULL,
     CONSTRAINT [PK_Set] PRIMARY KEY ([Id])
