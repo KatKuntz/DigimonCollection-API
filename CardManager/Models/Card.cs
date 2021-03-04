1a280eaf05b206fa2ff56f2f902b28c1c58d6b77
Kat Kuntz <kat.kuntz.dev@gmail.com>
2021-03-03 21:46:52 -0500

    Implement models
    Implement set model
    
    Implement card model
    
    Implement printing model

---
+++ CardManager/Models/Card.cs
@@ -0,0 +1,12 @@
+﻿namespace CardManager.Models
+{
+    class Card
+    {
+        public int CardId { get; set; }
+        public int SetId { get; set; }
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ CardManager/Models/Printing.cs
@@ -0,0 +1,9 @@
+﻿namespace CardManager.Models
+{
+    class Printing
+    {
+        public int PrintingId { get; set; }
+        public int CardId { get; set; }
+        public bool AlternateArt { get; set; }
+    }
+}
