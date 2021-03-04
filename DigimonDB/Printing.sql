0c8cb1ddccd8bc3aa98e47a7126c1c2c66ad28d2
Kat Kuntz <kat.kuntz.dev@gmail.com>
2021-03-03 21:25:13 -0500

    Add constraints to Printing table

--- DigimonDB/Printing.sql
+++ DigimonDB/Printing.sql
@@ -1,6 +1,9 @@
 ﻿CREATE TABLE [dbo].[Printing]
 (
-	[Id] INT NOT NULL  IDENTITY,
-    [AlternateArt] BIT NOT NULL DEFAULT 0,
-    CONSTRAINT [PK_Printing] PRIMARY KEY ([Id])
+    [Id] INT NOT NULL IDENTITY,
+    [CardId] INT NOT NULL,
+    [AlternateArt] BIT NOT NULL
+        CONSTRAINT [DF_Printing_AltArt] DEFAULT 0,
+    CONSTRAINT [PK_Printing] PRIMARY KEY ([Id]),
+    CONSTRAINT [UQ_Printing_CardId_AltArt] UNIQUE ([CardId],[AlternateArt])
 )
