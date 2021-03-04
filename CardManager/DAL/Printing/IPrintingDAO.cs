b2b99ceac9ac3caab5604536b0d3d14afee090d4
Kat Kuntz <kat.kuntz.dev@gmail.com>
2021-03-03 21:45:10 -0500

    Create DAO interfaces

---
+++ CardManager/DAL/Printing/IPrintingDAO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardManager.DAL.Printing
+{
+    interface IPrintingDAO
+    {
+    }
+}
---
+++ CardManager/DAL/Set/ISetDAO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardManager.DAL.Set
+{
+    class ISetDAO
+    {
+    }
+}
