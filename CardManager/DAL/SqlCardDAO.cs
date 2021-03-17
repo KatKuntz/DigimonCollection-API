01d25e218b1f2744348acaa3adf72da9a0bfdd11
Kat Kuntz <kat.kuntz.dev@gmail.com>
2021-03-17 17:14:03 -0400

    Implement sqlcarddao

--- CardManager/DAL/SqlCardDAO.cs
+++ CardManager/DAL/SqlCardDAO.cs
@@ -1,5 +1,5 @@
 ﻿using CardManager.Models;
-using System;
+using System.Data.SqlClient;
 
 namespace CardManager.DAL
 {
@@ -14,7 +14,23 @@ namespace CardManager.DAL
 
         public void AddCard(Card card)
         {
-            throw new NotImplementedException();
+            using SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+
+            string query = "INSERT INTO [dbo].[Card](Id, SetId, Name, Color, Type) " +
+                           "VALUES (@id, @setId, @name, @color, @type);";
+            
+            using SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddRange(new SqlParameter[]
+            {
+                new SqlParameter("@id", card.CardId),
+                new SqlParameter("@setId", card.SetId),
+                new SqlParameter("@name", card.Name),
+                new SqlParameter("@color", card.Color),
+                new SqlParameter("@type", card.Type)
+            });
+
+            cmd.ExecuteNonQuery();
         }
     }
 }
