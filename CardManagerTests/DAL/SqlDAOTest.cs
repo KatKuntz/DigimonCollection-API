5052ec3a4a74dfcdda84af5d70bdbb6aa13f9bce
Kat Kuntz <kat.kuntz.dev@gmail.com>
2021-03-17 03:33:25 -0400

    Add base sql dao test class

---
+++ CardManagerTests/DAL/SqlDAOTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Transactions;
+
+namespace CardManagerTests.DAL
+{
+    public abstract class SqlDAOTest
+    {
+        protected const string connectionString = "Server=(localdb)\\mssqllocaldb;Database=DigimonDB;Integrated Security=true";
+        private TransactionScope transaction;
+
+        [TestInitialize]
+        public void InitTest()
+        {
+            transaction = new TransactionScope();
+        }
+
+        [TestCleanup]
+        public void CleanupTest()
+        {
+            transaction.Dispose();
+        }
+    }
+}
