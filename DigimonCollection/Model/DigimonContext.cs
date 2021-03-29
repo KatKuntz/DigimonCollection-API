62b1a2258d3d57d2e78af943be4419b66d278d8d
Kat Kuntz <kat.kuntz.dev@gmail.com>
2021-03-29 19:54:41 -0400

    Setup asp.net and ef

---
+++ DigimonCollection/Model/DigimonContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DigimonCollection.Model
+{
+    public class DigimonContext : DbContext
+    {
+        public DigimonContext(DbContextOptions<DigimonContext> options)
+            : base(options)
+        {
+        }
+    }
+}
