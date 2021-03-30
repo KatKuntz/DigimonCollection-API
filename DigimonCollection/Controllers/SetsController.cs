12d0cbb2976a6cb682787171f7424b633c75d516
Kat Kuntz <kat.kuntz.dev@gmail.com>
2021-03-29 20:32:49 -0400

    Add test set controller

---
+++ DigimonCollection/Controllers/SetsController.cs
@@ -0,0 +1,24 @@
+﻿using DigimonCollection.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace DigimonCollection.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class SetsController : ControllerBase
+    {
+        private readonly DigimonContext context;
+
+        public SetsController(DigimonContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IQueryable<Set>> GetSets()
+        {
+            return Ok(context.Sets);
+        }
+    }
+}
--- DigimonCollection/Model/DigimonContext.cs
+++ DigimonCollection/Model/DigimonContext.cs
@@ -4,8 +4,8 @@ namespace DigimonCollection.Model
 {
     public class DigimonContext : DbContext
     {
-        public DbSet<Set> Sets { get; }
-        public DbSet<Card> Cards { get; }
+        public DbSet<Set> Sets { get; set; }
+        public DbSet<Card> Cards { get; set; }
         public DbSet<Printing> Printings { get; set; }
 
         public DigimonContext(DbContextOptions<DigimonContext> options)
